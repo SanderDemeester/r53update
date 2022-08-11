82caa299e57c349e579460d964ab79106140df66
EC2 Default User <ec2-user@ip-172-30-1-78.us-west-2.compute.internal>
2022-08-11 23:26:15 +0000

    use StaticCredential provider from credentialas loaded from ~/.aws/credentials

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"demees.local/r53update/utils"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"os"
+	"bufio"
+	"strings"
 )
 
 const (
@@ -22,8 +27,43 @@ func main() {
 		return
 	}
 
-	// get AWS session
-	session, err := session.NewSession()
+	// Read credentials from AWS credentials file
+	file, err := os.Open("/home/ec2-user/.aws/credentials")
+	
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	var match = false
+	var access_key_id = ""
+	var secret_access_key = ""
+
+	for scanner.Scan(){
+		line := scanner.Text()
+		
+		if(line == "[default]"){
+			match=true
+		}
+		if(match == true && strings.HasPrefix(line, "aws_access_key_id=")){
+			access_key_id = strings.Split(line, "=")[1]
+		}
+
+		if(match == true && strings.HasPrefix(line, "aws_secret_access_key=")){
+			secret_access_key = strings.Split(line, "=")[1]	
+			match=false // assume secret access key is the last in the config block - BAD CODE
+		}
+		
+	}
+	
+
+	// get AWS session - we are using the NewStaticCredentials on purpose 
+	session, err := session.NewSession(
+		&aws.Config{
+			Credentials: credentials.NewStaticCredentials(access_key_id,secret_access_key,""),
+		})
+
 	if err != nil {
 		fmt.Println(err)
 		return
